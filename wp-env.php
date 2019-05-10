9e65e6bd17455ac3102f2fd576c1c0e9844c1722
EC <crensoft@gmail.com>
2019-05-09 20:44:03 -0400

    Add wp-env.php documentation

--- wp-env.php
+++ wp-env.php
@@ -3,5 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/wp-content/env');
+
+if (file_exists(__DIR__ . '/wp-content/.env')) {
+  $dotenv = new Dotenv();
+  $dotenv->load(__DIR__ . '/wp-content/.env');
+}
